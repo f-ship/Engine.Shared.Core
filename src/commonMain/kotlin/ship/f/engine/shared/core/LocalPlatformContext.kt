6a95b67e310c45c247fe98e6bbe77887fe8b1638
Samuel Taiwo <samueltaiwo@stdev.co.uk>
2026-01-13 11:38:19 +0000

    [Implementation] Fixed video and webviews for iOS

--- src/commonMain/kotlin/ship/f/engine/shared/core/LocalPlatformContext.kt
+++
@@ -1,9 +0,0 @@
-package ship.f.engine.shared.core
-
-import androidx.compose.runtime.staticCompositionLocalOf
-
-expect class PlatformContext
-
-val LocalPlatformContext = staticCompositionLocalOf<PlatformContext> {
-    error("PlatformContext not provided")
-}
