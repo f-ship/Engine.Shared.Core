dab6cc1519b086e3d4066c27de69ebe2664fd48d
Samuel Taiwo <samueltaiwo@stdev.co.uk>
2026-02-12 23:20:53 +0000

    [Implementation] Improved the control flow of request in Zone to remove complexity

--- src/commonMain/kotlin/ship/f/engine/shared/core/ScopedEvent.kt
+++ src/commonMain/kotlin/ship/f/engine/shared/core/ScopedEvent.kt
@@ -3,13 +3,15 @@ package ship.f.engine.shared.core
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import ship.f.engine.shared.utils.serverdrivenui2.config.state.models.Id2
+import ship.f.engine.shared.utils.serverdrivenui2.config.state.models.Id2.Companion.NONE
 
 @Serializable
 @SerialName("ScopedEvent")
 abstract class ScopedEvent {
     @Serializable
     @SerialName("InitialEvent")
-    data class InitialEvent(val name: String) : ScopedEvent() { //I may be making an error with this scope implementation as well... Actually, I might be okay
+    data class InitialEvent(val name: String) :
+        ScopedEvent() { //I may be making an error with this scope implementation as well... Actually, I might be okay
         override fun getScopes2(): List<String> = listOf(defaultScope2) // TODO no one should care about the name for now
     }
 
@@ -38,7 +40,7 @@ abstract class ScopedEvent {
 
     @Serializable
     @SerialName("RequesterScopedEvent")
-    abstract class RequesterScopedEvent : ScopedEvent()  {
+    abstract class RequesterScopedEvent : ScopedEvent() {
         abstract val requesterId: String
     }
 
@@ -51,19 +53,37 @@ abstract class ScopedEvent {
         override fun getScopes2(): List<String> = listOf(defaultScope2)
         abstract fun plus(key: String, value: String): ViewRequest6
         abstract fun plus(key: String, value: List<String>): ViewRequest6
+
+        sealed class InitiatedViewRequest6 : ViewRequest6() {
+            abstract val requestId: String
+            abstract val domainIds: List<String>
+        }
     }
 
     @Serializable
-    @SerialName("InitiatedViewRequest6")
-    data class InitiatedViewRequest6(
+    data class DomainViewRequest6(
         override val id: Id2.MetaId2,
         override val ctx: Map<String, String>,
         override val listCtx: Map<String, List<String>>,
         override val requesterId: String,
-        val requestId: String,
+        override val requestId: String,
+        override val domainIds: List<String>,
         val domainId: String,
-        val domainIds: List<String>,
-    ) : ViewRequest6() {
+    ) : ViewRequest6.InitiatedViewRequest6() {
+        override fun plus(key: String, value: String) = copy(ctx = ctx + (key to value))
+        override fun plus(key: String, value: List<String>) = copy(listCtx = listCtx + (key to value))
+    }
+
+    @Serializable
+    @SerialName("InitiatedViewRequest6")
+    data class TrivialViewRequest6(
+        override val id: Id2.MetaId2,
+        override val ctx: Map<String, String>,
+        override val listCtx: Map<String, List<String>>,
+        override val requesterId: String,
+        override val requestId: String,
+        override val domainIds: List<String>,
+    ) : ViewRequest6.InitiatedViewRequest6() {
         override fun plus(key: String, value: String) = copy(ctx = ctx + (key to value))
         override fun plus(key: String, value: List<String>) = copy(listCtx = listCtx + (key to value))
     }
@@ -81,21 +101,33 @@ abstract class ScopedEvent {
     }
 
     abstract class DomainEvent6 : ScopedEvent() {
-        abstract val viewRequest: InitiatedViewRequest6
+        abstract val viewRequest: DomainViewRequest6
         abstract val domainId: String
         override fun getScopes2(): List<String> = listOf(defaultScope2)
     }
 
     abstract class FailedDomainEvent6 : ScopedEvent() {
-        abstract val viewRequest: InitiatedViewRequest6
+        abstract val viewRequest: DomainViewRequest6
         abstract val domainId: String
         override fun getScopes2(): List<String> = listOf(defaultScope2)
     }
 
     data class StaticVoid6(
-        override val viewRequest: InitiatedViewRequest6
+        override val viewRequest: DomainViewRequest6
     ) : DomainEvent6() {
         override val domainId: String = viewRequest.id.name + viewRequest.id.scope
+
+        companion object {
+            fun ViewRequest6.InitiatedViewRequest6.toComputingViewRequest6(domainId: String = NONE) = DomainViewRequest6(
+                id = id,
+                ctx = ctx,
+                listCtx = listCtx,
+                requesterId = requesterId,
+                requestId = requestId,
+                domainId = domainId,
+                domainIds = domainIds,
+            )
+        }
     }
 
     @Serializable
